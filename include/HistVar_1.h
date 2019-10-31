292438bdfa8c955947171e8430471df24e5b50eb
mc_cheng <38371018+dav7824@users.noreply.github.com>
2019-10-31 23:31:48 +0800

    add codes for filling histograms

---
+++ include/HistVar_1.h
@@ -0,0 +1,63 @@
+#ifndef _HISTVAR_1_H_
+#define _HISTVAR_1_H_
+
+enum histvar {
+    dipho_mass,
+    dipho_pt,
+    //////////
+    pho1_pt,
+    pho1_eta,
+    pho1_phi,
+    pho1_E,
+    pho1_idmva,
+    pho2_pt,
+    pho2_eta,
+    pho2_phi,
+    pho2_E,
+    pho2_idmva,
+    //////////
+    jet_N,
+    jet_pt,
+    jet_eta,
+    jet_phi,
+    jet_E,
+    jet_M,
+    //////////
+    met_pt,
+    met_phi,
+    met_px,
+    met_py,
+    met_SumET,
+    nhist
+};
+
+char unit[nhist][32] = {
+    "GeV",
+    "GeV",
+    //////////
+    "GeV",
+    "",
+    "",
+    "GeV",
+    "",
+    "GeV",
+    "",
+    "",
+    "GeV",
+    "",
+    //////////
+    "",
+    "GeV",
+    "",
+    "",
+    "GeV",
+    "GeV",
+    //////////
+    "GeV",
+    "",
+    "GeV",
+    "GeV",
+    "GeV"
+};
+
+#endif
---
+++ src/CreateHist_1-1.cc
@@ -0,0 +1,40 @@
+#include "HistVar.h"
+
+#include "TH1D.h"
+
+void CreateHist(TH1D **hist)
+{
+    hist[dipho_mass] = new TH1D("dipho_mass", ";diphoton mass (GeV);", 40, 100., 180.);
+    hist[dipho_pt] = new TH1D("dipho_pt", ";diphoton p_{T} (GeV);", 40, 0., 400.);
+
+    hist[pho1_pt] = new TH1D("pho1_pt", ";leading photon p_{T} (GeV);", 50, 0., 300.);
+    hist[pho1_eta] = new TH1D("pho1_eta", ";leading photon #eta;", 50, -3., 3.);
+    hist[pho1_phi] = new TH1D("pho1_phi", ";leading photon #phi;", 50, -3.5, 3.5);
+    hist[pho1_E] = new TH1D("pho1_E", ";leading photon E (GeV);", 50, 0., 600.);
+    hist[pho1_idmva] = new TH1D("pho1_idmva", ";leading photon IDMVA;", 50, -1., 1.);
+
+    hist[pho2_pt] = new TH1D("pho2_pt", ";subleading photon p_{T} (GeV);", 50, 0., 150.);
+    hist[pho2_eta] = new TH1D("pho2_eta", ";subleading photon #eta;", 50, -3., 3.);
+    hist[pho2_phi] = new TH1D("pho2_phi", ";subleading photon #phi;", 50, -3.5, 3.5);
+    hist[pho2_E] = new TH1D("pho2_E", ";subleading photon E (GeV);", 50, 0., 300.);
+    hist[pho2_idmva] = new TH1D("pho2_idmva", ";subleading photon IDMVA;", 50, -1., 1.);
+
+    hist[jet_N] = new TH1D("jet_N", ";number of jets;", 12, -0.5, 11.5);
+    hist[jet_pt] = new TH1D("jet_pt", ";jet p_{T} (GeV);", 50, 0., 300.);
+    hist[jet_eta] = new TH1D("jet_eta", ";jet #eta;", 50, -3., 3.);
+    hist[jet_phi] = new TH1D("jet_phi", ";jet #phi;", 50, -3.5, 3.5);
+    hist[jet_E] = new TH1D("jet_E", ";jet E (GeV);", 50, 0., 600.);
+    hist[jet_M] = new TH1D("jet_M", ";jet mass (GeV);", 50, 0., 50.);
+
+    hist[met_pt] = new TH1D("met_pt", ";MET p_{T} (GeV);", 50, 0., 200.);
+    hist[met_phi] = new TH1D("met_phi", ";MET #phi;", 50, -3.5, 3.5);
+    hist[met_px] = new TH1D("met_px", ";MET p_{x} (GeV);", 50, -200., 200.);
+    hist[met_py] = new TH1D("met_py", ";MET p_{y} (GeV);", 50, -200., 200.);
+    hist[met_SumET] = new TH1D("met_SumET", ";MET sum E_{T} (GeV);", 50, 0., 4000.);
+
+    for (int i=0; i<nhist; ++i) {
+	double width = (hist[i]->GetXaxis()->GetXmax() - hist[i]->GetXaxis()->GetXmin()) / hist[i]->GetNbinsX();
+	hist[i]->SetYTitle( Form("events / %.2g %s", width, unit[i]) );
+	hist[i]->Sumw2();
+    }
+}
