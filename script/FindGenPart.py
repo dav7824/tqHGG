86bfb3b8e0bf639704b6f88714ca72dd653befd3
mc_cheng <38371018+dav7824@users.noreply.github.com>
2021-02-12 22:44:02 +0800

    find gen-particle index

--- script/FindGenPart.py
+++ script/FindGenPart.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python2
 
-import Path, Util, Samples
+import Path, Util
+from Samples import sig_MC
 import sys, os
-from os.path import join
+from os.path import join, exists
 
-exe = join(Path.dir_bin, 'FindGenPart')
-indir = join(Path.dir_2017, 'MVAreco_GenPerm')
 
-cmd_template = '{bin} {{fin}} T {{type}} {{ch}}'.format(bin=exe)
+def FindGenPart(indir_name, ch):
+    exe = join(Path.dir_bin, 'FindGenPart')
+    indir = join(Path.dir_2017, indir_name)
+    if ch!='had' and ch!='lep':
+        print '[ERROR] Invalid channel'
+        sys.exit(1)
+    if not exists(exe):
+        print '[ERROR] Executable not exist!'
+        sys.exit(1)
+    if not exists(indir):
+        print '[ERROR] Input dir not exist!'
+        sys.exit(1)
 
-#os.system(cmd_template.format(fin=join(indir,'TT_FCNC-TtoHJ_aThad_hct.root'), type='TT', ch='had'))
+    # Command template
+    cmd = '{exe} {indir}/{{nt}}.root {tree} {{type}} {ch}'.format(
+            exe=exe, indir=indir, tree='T', ch=ch)
 
-for key in Samples.sig_MC_s:
-    for nt in Samples.sig_MC_s[key]:
-        print 'Processing:', nt
-        fp = os.popen( cmd_template.format(fin=join(indir,nt+'.root'), type=key[0], ch=key[1]) )
-        print fp.read()
-        fp.close()
+    # Process signal MC
+    for sigtype in sig_MC:
+        if ch != sigtype[1]:
+            continue
+        for nt in sig_MC[sigtype]:
+            print '---Processing:', nt
+            fp = os.popen( cmd.format(nt=nt, type=sigtype[0]) )
+            print fp.read()
+            fp.close()
+# End of function FindGenPart
 
-print 'End!'
+
+if __name__ == '__main__':
+    #FindGenPart( 'Presel_had_phID_btag-L', 'had' )
+    FindGenPart( 'Presel_lep_phID', 'lep' )
